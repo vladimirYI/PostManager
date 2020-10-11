f20eefaf48bbc51ea28fa39c6ed6f4d3be3098c6
Vladimir <torna7@yandex.ru>
2020-10-11 18:31:22 +0300

    the ability to delete posts

--- src/components/post-list/post-list.js
+++ src/components/post-list/post-list.js
@@ -1,22 +1,25 @@
 import React from 'react';
 import PostListItem from '../post-list-item';
+import {ListGroup} from 'reactstrap';
 import './post-list.css';
 
-const PostList = ({posts}) => {
+const PostList = ({posts, onDelete}) => {
 
-    const elements = posts.map((item) => {
+    const elements = posts.map( (item) => {
         const {id, ...itemProps} = item;
         return (
             <li key={id} className='list-group-item'>
-                <PostListItem {...itemProps}/>
+                <PostListItem 
+                    {...itemProps}
+                    onDelete={() => onDelete(id)}/>
             </li>
-        )
+        ) 
     });
 
     return (
-        <ul className = "app-list list-group">
+        <ListGroup className = "app-list">
             {elements}
-        </ul>
+        </ListGroup>
     )
 }
 
